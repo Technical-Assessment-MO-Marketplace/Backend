dd91ddf493927d828f3aa3bf5934cf2c6ae391aa
Wimukthi_madushan <bandarawgwm.21@uom.lk>
2026-04-02 20:22:08 +0530

    Enable CORS with specified origins and headers

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'http://localhost:5174',
+      'http://127.0.0.1:5173',
+      'http://127.0.0.1:3000',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
